55a5a4b0ef0bc1f6685e4f10d41c0c734a5d2d04
HohyunSon <tpfkajsl98@naver.com>
2023-08-01 17:36:55 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		long a=0,b=0,c=0;
+		a = sc.nextLong();
+		b = sc.nextLong();
+		c = sc.nextLong();
+
+		System.out.println(a+b+c);
+
+	}
+}
