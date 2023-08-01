debf1717460a515e7529f4de526db007df64c68e
HohyunSon <tpfkajsl98@naver.com>
2023-08-01 17:31:43 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 18032 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double num1=0,num2=0;
+		num1 = sc.nextDouble();
+		num2 = sc.nextDouble();
+		System.out.print(num1/num2);
+	}
+}
